b2327ffcaac3328dd0717361a208fb42ae99e814
atul164atul <atuldenny2004@gmail.com>
2023-12-19 14:54:22 +0530

    bnmbn

--- testc.c
+++ testc.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 int main(){
     printf("Hello world");
+    int a=2 , b=3;
+int sum=a+b;
     return 0;
 }
---
+++ tester.c
@@ -0,0 +1,7 @@
+//hello
+//int add(int,int);
+
+
+/*int add(int z,int x){
+    hsdjaj
+}*/
